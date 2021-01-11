465f49b17a6b03cfa6dc672ae7c62f31a5b511dd
fox <hello@vanutp.dev>
2021-01-12 00:10:29 +0300

    Add test for data_encoding

--- tests/test_cpp.py
+++ tests/test_cpp.py
@@ -29,3 +29,18 @@ int main()
         res = await self.tester.run(test_program, 'cpp', '2')
         self.assertEqual(res.status, ExecStatus.CE)
         self.assertEqual(res.compiler_message, 'Compilation timed out')
+
+    async def test_encoding(self):
+        test_program = '''#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+  string text;
+  cin >> text;
+  unsigned char a = text[0];
+  cout << int(a) << endl;
+}'''
+        res = await self.tester.run(test_program, 'cpp', 'а', data_encoding='cp1251')
+        self.assertEqual(res.status, ExecStatus.OK)
+        self.assertEqual(res.stdout, '224')
