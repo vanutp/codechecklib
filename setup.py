2f9c29832bdc6af980eda69cef0246e845a6c9e3
vanutp <ivan51673@gmail.com>
2021-03-27 17:55:02 +0000

    v0.3.13.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name='codechecklib',
-    version='0.3.13',
+    version='v0.3.13.1',
     author='vanutp',
     author_email='hello@vanutp.dev',
     description='Simple sandbox & testing utility',
